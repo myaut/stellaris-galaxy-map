a56db7e288f82632db155a8e7cb5b2a55ebb5f1a
Sergey Klyaus <myautneko@gmail.com>
2022-05-23 02:37:04 +0300

    timelapses

--- cmd/stellaris-galaxy-map-cli/main.go
+++ cmd/stellaris-galaxy-map-cli/main.go
@@ -1,35 +1,147 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+
+	"github.com/fsnotify/fsnotify"
 
 	"github.com/myaut/stellaris-galaxy-map/pkg/sgm"
 	"github.com/myaut/stellaris-galaxy-map/pkg/sgmrender"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("Invalid number of arguments: 1 is expected")
+func getSaveLocation() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
 	}
 
-	fileName := os.Args[1]
+	switch runtime.GOOS {
+	case "windows":
+		return filepath.Join(homeDir, `Documents\Paradox Interactive\Stellaris\save games`)
+	case "linux":
+		return filepath.Join(homeDir, ".local/share/Paradox Interactive/Stellaris/save games")
+	case "darwin":
+		return filepath.Join(homeDir, "Documents/Paradox Interactive/Stellaris/save games/")
+	}
+
+	panic("Unsupported platform")
+}
+
+func getOutFileName(fileName string) (string, error) {
 	ext := filepath.Ext(fileName)
 	if ext != ".sav" {
-		log.Fatal("Unexpected file as input, it should have .sav extension")
+		return "", fmt.Errorf("unexpected file '%s' as input, it should have .sav extension", fileName)
 	}
-	outFileName := fileName[:len(fileName)-len(ext)] + ".svg"
 
+	return fileName[:len(fileName)-len(ext)] + ".svg", nil
+}
+
+func renderFile(fileName, outFileName string) error {
 	state, err := sgm.LoadGameState(fileName)
 	if err != nil {
-		log.Fatal(err.Error())
+		return err
 	}
 
 	r := sgmrender.NewRenderer(state)
 	r.Render()
-	err = r.Write(outFileName)
+	return r.Write(outFileName)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		runBackground()
+		return
+	}
+
+	fileName := os.Args[1]
+	outFileName, err := getOutFileName(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = renderFile(fileName, outFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runBackground() {
+	saveDir := askEmpireDir()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(saveDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("waiting for save games in %s\n", saveDir)
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				ext := filepath.Ext(event.Name)
+				if ext == ".svg" {
+					continue
+				}
+
+				log.Println("got a save game:", event.Name)
+				outFileName, err := getOutFileName(event.Name)
+				if err != nil {
+					log.Println(err)
+				}
+
+				err = renderFile(event.Name, outFileName)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
+}
+
+func askEmpireDir() string {
+	saveRootDir := getSaveLocation()
+	empireDirs, err := ioutil.ReadDir(saveRootDir)
+
+	for idx, dir := range empireDirs {
+		if !dir.IsDir() {
+			continue
+		}
+
+		fmt.Printf("[%2d] - %s\n", idx+1, dir.Name())
+	}
+
+	fmt.Println("Select an empire:")
+
+	var empireIdx int
+	_, err = fmt.Scanf("%d", &empireIdx)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
+	empireIdx--
+	if empireIdx < 0 || empireIdx > len(empireDirs) {
+		log.Fatalf("invalid empire #%d\n", empireIdx+1)
+	}
+
+	return filepath.Join(saveRootDir, empireDirs[empireIdx].Name())
 }
